bde03df8e21a02e6f2c7ab9fcd4a2e0a2e6d26a7
thisisyahyaRender <websiteuionfiverr@gmail.com>
2025-12-11 11:13:18 +0500

    Remove naqi and yahya files from awais branch

--- naqi(43).cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "This branch is using NAQI's code." << endl;
-    return 0;
-}
--- yahya(33).cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "This branch is using YAHYA's code." << endl;
-
-    cout<<":i have updated htus coce but i am not pushing it";
-    return 0;
-}
